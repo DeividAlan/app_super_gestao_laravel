51cd6b1512d217c15d1d00c59b07f2f77d7c955e
Deivid Alan <deividalan@hotmail.com>
2022-11-02 17:44:43 -0300

    commit

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -8,9 +8,18 @@ class FornecedorController extends Controller
 {
     public function index() {
         $fornecedores = [
-            0 => ['nome' => 'Fornecedor 1', 'status' => 'N'],
-            1 => ['nome' => 'Fornecedor 2', 'status' => 'S']
+            0 => [
+                'nome' => 'Fornecedor 1',
+                'status' => 'N',
+                'cnpj' => '00.000.000/000-00'
+            ],
+            1 => [
+                'nome' => 'Fornecedor 2',
+                'status' => 'S'
+            ],
         ];
+
+        // $fornecedores = null;
 
         return view('app.fornecedor.index', compact('fornecedores'));
     }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,32 +1,23 @@
 <h3>Fornecedor</h3>
 
 
-
-
-{{-- Outra Forma Comentario --}}
-
 @php
     /*
-        if() {
-
-        } elseif() {
-
-        } else {
-
-        }
+        if(isset($variavel)) {} //retorna true se a variavel extiver definida
     */
 @endphp
 
 {{-- @dd($fornecedores) --}}
 
-Fornecedor: {{ $fornecedores[0]['nome'] }}
-<br>
-Status: {{ $fornecedores[0]['status'] }}
-<br>
-@if( !($fornecedores[0]['status'] == 'S') )
-    Fornecedor inativo
-@endif
-<br>
-@unless($fornecedores[0]['status'] == 'S')
-    Fornecedor inativo
-@endunless
+
+@isset($fornecedores)
+    Fornecedor: {{ $fornecedores[1]['nome'] }}
+    <br>
+    Status: {{ $fornecedores[1]['status'] }}
+    <br>
+    @isset($fornecedores[1]['cnpj'])
+        Cnpj: {{ $fornecedores[1]['cnpj'] }}
+    @endisset
+@endisset
+
+
