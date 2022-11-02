f12dd0d761bddab969c28aecf730580017f1498a
Deivid Alan <deividalan@hotmail.com>
2022-11-02 16:54:19 -0300

    commit

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,4 +1,29 @@
 <h3>Fornecedor</h3>
 
+
+
 <!-- Comentario -->
 {{-- Outra Forma Comentario --}}
+
+@php
+    /*
+        if() {
+
+        } elseif() {
+
+        } else {
+
+        }
+    */
+@endphp
+
+@dd($fornecedores)
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif (count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados</h3>
+@else
+    <h3>Ainda não exitem fornecedores cadastrados</h3>
+@endif
+
