c87671b947932cd551a0b680579c5ef4386a6a15
Deivid Alan <deividalan@hotmail.com>
2022-11-06 10:04:36 -0300

    commit

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -11,26 +11,20 @@
 
 
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[1]['nome'] }}
-    <br>
-    Status: {{ $fornecedores[1]['status'] }}
-    <br>
-    Cnpj: {{ $fornecedores[1]['cnpj'] ?? '- vazio' }}
-    <br>
-    Telefone: ({{ $fornecedores[1]['ddd'] ?? '- vazio' }}) {{ $fornecedores[1]['telefone'] ?? '- vazio' }}
-    @switch($fornecedores[1]['ddd'])
-        @case('11')
-            São Paulo - SP
-            @break
-        @case('32')
-            Juiz de Fora - MG
-            @break
-        @case('85')
-            Fortaleza - CE
-            @break
-        @default
-            Estado não identificado
-    @endswitch
+    @for($i = 0; isset($fornecedores[$i]); $i++)
+        Fornecedor: {{ $fornecedores[$i]['nome'] }}
+        <br>
+        Status: {{ $fornecedores[$i]['status'] }}
+        <br>
+        Cnpj: {{ $fornecedores[$i]['cnpj'] ?? '- vazio' }}
+        <br>
+        Telefone: ({{ $fornecedores[$i]['ddd'] ?? '- vazio' }}) {{ $fornecedores[1]['telefone'] ?? '- vazio' }}
+        <hr>
+    @endfor
 @endisset
+
+<br>
+
+
 
 
